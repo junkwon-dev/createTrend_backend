8701f789501554ceaf96bd12ea50d76412faa7be
junkwon <dydqja1013@naver.com>
2020-08-08 19:24:30 +0900

    Add app Search_StarYoutuber

---
+++ createTrend/Search_Keyword/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from Search_Keyword import views
+
+urlpatterns = [
+    path('hotvideos/', views.hotvideos),
+    path('topKeywords/', views.topKeywords),
+    path('topImaging/', views.topImaging),
+]
---
+++ createTrend/Search_StarYoutuber/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SearchStaryoutuberConfig(AppConfig):
+    name = 'Search_StarYoutuber'
