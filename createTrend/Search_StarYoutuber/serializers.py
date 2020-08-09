6ad04ff561fe754bd016cc7911ba7cf454e5acc6
junkwon <dydqja1013@naver.com>
2020-08-10 01:06:59 +0900

    Edit app Search_StarYoutuber
    change query

--- createTrend/Search_StarYoutuber/serializers.py
+++ createTrend/Search_StarYoutuber/serializers.py
@@ -6,10 +6,10 @@ from .models import Channel, ChannelSubscriber
 class SubscriberNumberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ChannelSubscriber
-        fields = ['subscriber_num','check_time']
+        fields = ['check_time','subscriber_num']
 
 class ChannelInfoSerializer(serializers.HyperlinkedModelSerializer):  
-    channel_subscriber=SubscriberNumberSerializer(many=True, read_only = True)
+    channelsubscriber=SubscriberNumberSerializer(many=True, read_only = True)
     class Meta:
         model = Channel
-        fields = ('thumbnail_url', 'channel_description', 'channel_name', 'channel_start_date','channel_subscriber')
+        fields = ['thumbnail_url', 'channel_description', 'channel_name', 'channel_start_date','channelsubscriber']
