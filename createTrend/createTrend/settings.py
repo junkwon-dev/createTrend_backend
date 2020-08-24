a0089910926583e8d22911b6d02728fed1df55dc
junkwon <dydqja1013@naver.com>
2020-08-24 13:28:08 +0900

    Add drf_yasg to settings.py

--- createTrend/createTrend/settings.py
+++ createTrend/createTrend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'Search_StarYoutuber',
     'corsheaders',
     'Analyze_Channel',
+    'drf_yasg',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
