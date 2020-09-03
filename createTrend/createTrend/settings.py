88ace959add6d1839aa743089fedd2b206b4d1f4
junkwon <dydqja1013@naver.com>
2020-09-03 14:18:50 +0900

    Add docker-compose

--- createTrend/createTrend/settings.py
+++ createTrend/createTrend/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
