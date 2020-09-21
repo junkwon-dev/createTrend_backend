8b378a7bc62e31ba42e4d10277e4b54d64d4ee28
jun <dydqja1013@naver.com>
2020-09-21 16:32:47 +0900

    Add logout

--- createTrend/accounts/urls.py
+++ createTrend/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.conf.urls import url
 from .views import RegistrationAPI, LoginAPI, UserAPI, UserInfoUpdateAPI
-
+from knox import views as knox_views
 urlpatterns = [
     path("auth/register/", RegistrationAPI.as_view()),
     path("auth/login/", LoginAPI.as_view()),
     path("auth/user/", UserAPI.as_view()),
     path('auth/userinfo/update/', UserInfoUpdateAPI.as_view()),
+    path("auth/logout/", knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
