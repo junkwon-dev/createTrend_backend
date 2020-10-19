95f13113c9a59542f9c4cd934c8355ca48d06d74
junkwon <dydqja1013@naver.com>
2020-10-19 13:34:49 +0900

    Add Video views predict templates

--- createTrend/Views_Predict/views.py
+++ createTrend/Views_Predict/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def videoPredict(request,pk):
+    if request.method == 'GET':
+        thumbnail_url = request.query_params.get("thumbnail_url")
+        video_name = request.query_params.get("video_name")
+        channel_subscriber = request.query_params.get("channel_subscriber")
+        upload_date = request.query_params.get("upload_date")
+        if thumbnail_url and video_name and channel_subscriber and upload_date:
+            return Response()
+        else:
+            return Response()
+            
