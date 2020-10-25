830c8ab225c56c5132b48f8c45ac0b84d512f7e0
junkwon <dydqja1013@naver.com>
2020-10-25 19:17:56 +0900

    Edit elasticsearch test view

--- createTrend/test_elasticsearch/views.py
+++ createTrend/test_elasticsearch/views.py
@@ -7,11 +7,16 @@ from rest_framework.decorators import api_view
 # Create your views here.
 @api_view(["GET"])
 def keyword(request):
-    s=VideoDocument.search().query("term",videokeywordnews__keyword="챌린저")
+    search = request.query_params.get("search")
+    s=VideoDocument.search().query("term",videokeywordnews__keyword=search)
+    returnlist=[]
     for hit in s:
         print(
             "Video name : {}".format(hit.video_name)
         )
+        keywordlist=[]
         for video_keyword in hit.videokeywordnews:
             print(video_keyword)
-    return Response({'a':'b'})
+            keywordlist.append({'keyword':video_keyword})
+        returnlist.append({"video_name" : hit.video_name,"keywordlist":keywordlist})
+    return Response(returnlist)
